bed5dd694c64aa9ceb9f4f4dd5ebf199bf113383
Owais Islam <66886795+OwaisIslam@users.noreply.github.com>
2021-01-15 14:58:01 -0600

    added success page to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Success from "./pages/Success";
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
@@ -33,26 +34,11 @@ function App() {
 						<Nav />
 						<Switch>
 							<Route exact path="/" component={Home} />
-							<Route
-								exact
-								path="/login"
-								component={Login}
-							/>
-							<Route
-								exact
-								path="/signup"
-								component={Signup}
-							/>
-							<Route
-								exact
-								path="/orderHistory"
-								component={OrderHistory}
-							/>
-							<Route
-								exact
-								path="/products/:id"
-								component={Detail}
-							/>
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/signup" component={Signup} />
+							<Route exact path="/orderHistory" component={OrderHistory} />
+							<Route exact path="/products/:id" component={Detail} />
+							<Route exact path="/success" component={Success} />
 							<Route component={NoMatch} />
 						</Switch>
 					</StoreProvider>
